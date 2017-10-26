2465ae0b9b27a2ab093e9f13950714cd9a2a47b1
Alex <lepsalex@gmail.com>
2017-10-26 15:03:59 -0400

    Form bootstrap cleanup

--- enrolment-ui/src/modules/Projects/components/ReqFormStep1.js
+++ enrolment-ui/src/modules/Projects/components/ReqFormStep1.js
@@ -6,8 +6,12 @@ const ReqFormStep1 = props => {
   const { handleSubmit } = props;
   return (
     <form onSubmit={handleSubmit}>
-      <h2 className="fs-title">Principal Investigator</h2>
-      <h3 className="fs-subtitle">Personal information regarding your role</h3>
+      <div className="row">
+        <div className="col-md-12">
+          <h2 className="fs-title">Principal Investigator</h2>
+          <h3 className="fs-subtitle">Personal information regarding your role</h3>
+        </div>
+      </div>
       <div className="row">
         <div className="col-md-6">
           <Field name="firstname" type="text" placeholder="First Name" component={RFPlainInput} />
@@ -15,33 +19,51 @@ const ReqFormStep1 = props => {
         <div className="col-md-6">
           <Field name="lastname" type="text" placeholder="Last Name" component={RFPlainInput} />
         </div>
-        <div>
+      </div>
+      <div className="row">
+        <div className="col-md-12">
           <Field name="position" type="text" placeholder="Position" component={RFPlainInput} />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-12">
           <Field
             type="text"
             name="institution_name"
             placeholder="Institution Name"
             component={RFPlainInput}
           />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-12">
           <Field type="text" name="address" placeholder="Address" component={RFPlainInput} />
-          <div className="row">
-            <div className="col-md-6">
-              <Field
-                type="email"
-                name="institution_email"
-                placeholder="Institution Email"
-                component={RFPlainInput}
-              />
-            </div>
-            <div className="col-md-6">
-              <Field type="tel" name="phone" placeholder="Phone" component={RFPlainInput} />
-            </div>
-          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-6">
+          <Field
+            type="email"
+            name="institution_email"
+            placeholder="Institution Email"
+            component={RFPlainInput}
+          />
+        </div>
+        <div className="col-md-6">
+          <Field type="tel" name="phone" placeholder="Phone" component={RFPlainInput} />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-12">
           <Field type="email" name="daco_email" placeholder="Daco Email" component={RFPlainInput} />
         </div>
-        <button type="submit" className="next action-button">
-          Next
-        </button>
+      </div>
+      <div className="row">
+        <div className="col-md-12">
+          <button type="submit" className="next action-button">
+            Next
+          </button>
+        </div>
       </div>
     </form>
   );
--- enrolment-ui/src/modules/Projects/components/ReqFormStep2.js
+++ enrolment-ui/src/modules/Projects/components/ReqFormStep2.js
@@ -6,19 +6,40 @@ const ReqFormStep2 = props => {
   const { handleSubmit, previousPage } = props;
   return (
     <form onSubmit={handleSubmit}>
-      <h2 className="fs-title">Collaboratory Project</h2>
-      <h3 className="fs-subtitle">
-        Please provide a name and short description of the research project you are requesting
-        Collaboratory’s compute resources for.
-      </h3>
-      <Field type="text" name="project_name" placeholder="Project Name" component={RFPlainInput} />
-      <textarea name="project_description" placeholder="Project Description" />
-      <button onClick={previousPage} className="previous action-button">
-        Previous
-      </button>
-      <button type="submit" className="next action-button">
-        Next
-      </button>
+      <div className="row">
+        <div className="col-md-12">
+          <h2 className="fs-title">Collaboratory Project</h2>
+          <h3 className="fs-subtitle">
+            Please provide a name and short description of the research project you are requesting
+            Collaboratory’s compute resources for.
+          </h3>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-12">
+          <Field
+            type="text"
+            name="project_name"
+            placeholder="Project Name"
+            component={RFPlainInput}
+          />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-12">
+          <textarea name="project_description" placeholder="Project Description" />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-12">
+          <button onClick={previousPage} className="previous action-button">
+            Previous
+          </button>
+          <button type="submit" className="next action-button">
+            Next
+          </button>
+        </div>
+      </div>
     </form>
   );
 };
