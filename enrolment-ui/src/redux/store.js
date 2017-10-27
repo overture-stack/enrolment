1cf45e45b8115da87e029c6e4106065d56e0c632
Alex <lepsalex@gmail.com>
2017-10-27 15:41:06 -0400

    Presisting redux state in localStorage

--- enrolment-ui/src/redux/store.js
+++ enrolment-ui/src/redux/store.js
@@ -1,11 +1,13 @@
 import { compose, applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
+import persistState from 'redux-localstorage';
 
 import reducer from './reducer';
 
 const composedStore = compose(
   applyMiddleware(thunk),
   window.devToolsExtension ? window.devToolsExtension() : f => f,
+  persistState(),
 )(createStore);
 
 const configureStore = initialState => composedStore(reducer, initialState);
