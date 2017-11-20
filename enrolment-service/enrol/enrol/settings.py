748fe211c962927d828c160179db709dc31155bf
Alex <lepsalex@gmail.com>
2017-11-20 16:02:03 -0500

    Docker Compose working - db persistence/backup next

--- enrolment-service/enrol/enrol/settings.py
+++ enrolment-service/enrol/enrol/settings.py
@@ -174,7 +174,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 SITE_ID = 1
 
 
--- enrolment-service/enrol/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Copyright 2017(c) The Ontario Institute for Cancer Research. All rights reserved.
-
-service nginx restart
-source /srv/enrolment-service/env/bin/activate
-
-gunicorn -w 6 -b 0.0.0.0:8000 --chdir /srv/enrolment-service/enrol enrol.wsgi --access-logfile=/var/log/gunicorn/access.log --error-logfile=/var/log/gunicorn/error.log
