b0c87e0345b6424771a3eb8046b8faa19bcd6c32
Alex <lepsalex@gmail.com>
2017-11-23 14:09:23 -0500

    Project Enrolment - Terms and Conditions #36

--- enrolment-ui/src/config/config.template.js
+++ enrolment-ui/src/config/config.template.js
@@ -1,5 +1,7 @@
 // ** Clone this file as config.js, replace values as needed ** //
 
 export default {
-    clientId: 'OAuth 2.0 client ID from https://console.cloud.google.com/apis/credentials?project=##YOUR_PROJECT_NAME##'
-}
+  clientId:
+    'OAuth 2.0 client ID from https://console.cloud.google.com/apis/credentials?project=##YOUR_PROJECT_NAME##',
+  peFullTerms: 'https://link.to/terms',
+};
