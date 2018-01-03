5a749ba2bdb26ed8c72666fd5cc7249ed3f273ce
Alex <lepsalex@gmail.com>
2018-01-03 16:03:19 -0500

    Billing API endpoint update (back to normal)

--- enrolment-ui/src/services/async.js
+++ enrolment-ui/src/services/async.js
@@ -69,7 +69,7 @@ export function createAsyncs(isDevelopment = false) {
         }),
     },
     billing: {
-      getPrice: asyncServiceCreator('GET', `${billingBase}/price?date=2017-12-08`),
+      getPrice: asyncServiceCreator('GET', `${billingBase}/price`),
     },
   };
 
