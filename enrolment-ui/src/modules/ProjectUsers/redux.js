eee40674e9866a4bd6836ae3c7e0bf7de927b438
Xu Deng <d8660091@gmail.com>
2018-11-28 09:54:41 -0500

    Retain uniqueProjectUser check

--- enrolment-ui/src/modules/ProjectUsers/redux.js
+++ enrolment-ui/src/modules/ProjectUsers/redux.js
@@ -163,20 +163,18 @@ export function activateProjectUser(dispatch, projectId, id, next = () => null) 
 export function emailCheck(dispatch, project, email) {
   dispatch(emailCheckStart());
 
-  // Email check placeholder
-  dispatch(emailCheckSuccess(email));
-
-  // We first check to ensure that the email is not already in use,
-  // then we check that it's a valid daco email
-  /* return asyncServices.uniqueProjectUser
-   *   .check(project, email)
-   *   .then(() => asyncServices.daco.check(email))
-   *   .then(response => {
-   *     dispatch(emailCheckSuccess(email));
-   *   })
-   *   .catch(error => {
-   *     dispatch(emailCheckError(error));
-   *   }); */
+  // Ensure that the email is not already in use
+  return (
+    asyncServices.uniqueProjectUser
+      .check(project, email)
+      /*     .then(() => asyncServices.daco.check(email)) */
+      .then(response => {
+        dispatch(emailCheckSuccess(email));
+      })
+      .catch(error => {
+        dispatch(emailCheckError(error));
+      })
+  );
 }
 
 /*
